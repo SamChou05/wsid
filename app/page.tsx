c56caff8e00dc3d15c0c9291ebf3149664936901
Sam Chou <sam@thechous.com>
2023-11-24 22:36:08 -0800

    add

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import { TypeAnimation } from "react-type-animation";
 import Link from 'next/link';
 
-//test 
+//testsetadf
 
 export default function Home() {
 
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className=" bg-gray w-full h-[1024px] overflow-hidden text-left text-5xl text-black font-raleway">
       <Link href = 'first-choice' className='p-0 bg-[transparent] absolute top-[594px] left-[631px] text-[33px] font-raleway text-black text-left inline-block w-[179px] h-[38px]'>
-        get started
+        get started 1
       </Link>
     </div>
   )
